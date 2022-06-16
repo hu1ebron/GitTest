54d5a69ef61ee294415d3e351ca60112089dc7b3
hulebron <hulebron@163.com>
2022-06-16 18:31:28 +0800

    hot-fix commit

--- src/main/java/com/hulebron/git/test1.java
+++ src/main/java/com/hulebron/git/test1.java
@@ -1,8 +1,10 @@
 package com.hulebron.git;
 
 public class test1 {
+
     public static void main(String[] args) {
         System.out.println("gitTest");
         System.out.println("gitTest2");
+        System.out.println("hot-fix");
     }
 }
