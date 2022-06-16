fd197f13b1de3e0f2b18f76282cc887924bfa1d9
hulebron <hulebron@163.com>
2022-06-16 17:35:46 +0800

    second commit

--- src/main/java/com/hulebron/git/test1.java
+++ src/main/java/com/hulebron/git/test1.java
@@ -3,5 +3,6 @@ package com.hulebron.git;
 public class test1 {
     public static void main(String[] args) {
         System.out.println("gitTest");
+        System.out.println("gitTest2");
     }
 }
