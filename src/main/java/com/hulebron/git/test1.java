67e5bf96c53dc6169bdcfd96211e4fd2a7e2df95
hulebron <hulebron@163.com>
2022-06-16 19:14:18 +0800

    push commit

--- src/main/java/com/hulebron/git/test1.java
+++ src/main/java/com/hulebron/git/test1.java
@@ -8,5 +8,6 @@ public class test1 {
         System.out.println("hot-fix");
         System.out.println("master");
         System.out.println("hot-fix2");
+        System.out.println("push test");
     }
 }
